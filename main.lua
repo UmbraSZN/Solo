3adfd263347ec42ef9adc0a92c59e34de3884b0b
Umbra <loganfmacmillan@gmail.com>
2023-02-09 00:10:56 +0000

    Added collision classes

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ function love.load()
 
     local testCollider = world:newRectangleCollider(100, 150, 80, 200)
     testCollider:setType("static")
+    testCollider:setCollisionClass("Wall")
 
 end
 
--- src/player.lua
+++ src/player.lua
@@ -1,4 +1,5 @@
 player = world:newCircleCollider(50, 50, 20)
+player:setCollisionClass("Player")
 player.speed = 80
 
 function player:update(dt)
