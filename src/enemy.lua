96058db261e4b1aa93a44da652841d9ddaaa2e53
Umbra <loganfmacmillan@gmail.com>
2023-03-08 23:06:06 +0000

    Added spawning function

--- src/enemy.lua
+++ src/enemy.lua
@@ -1,1 +1,18 @@
 enemies = {}
+
+function enemies:spawn(x, y, type) --spawn a new enemy
+
+    local enemy
+    if type == "test" then
+        enemy = world:newCircleCollider(x, y, 20)
+        
+    elseif type == "" then
+        enemy = world:newCircleCollider(x, y, 20)
+
+    end
+
+    enemy:setCollisionClass("Enemy")
+end
+
+
+
