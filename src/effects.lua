44e2570976b177c6ebced0c6c6b1c99d690be69d
Umbra <loganfmacmillan@gmail.com>
2023-03-16 21:42:40 +0000

    Added effects file

---
+++ src/effects.lua
@@ -0,0 +1,39 @@
+effects = {}
+
+function effects:spawn(x, y, type)
+    
+    local effect = {}
+    effect.x = x 
+    effect.y = y
+    effect.type = type
+
+    if type == "" then
+
+
+    elseif type == "" then
+
+    end
+
+    
+    table.insert(effects, effect)
+end
+
+
+function effects:update(dt)
+
+    for _, e in ipairs(effects) do
+        
+    end
+end
+
+function effects:draw()
+
+    for _, e in ipairs(effects) do
+        
+    end
+end
+
+
+
+
+
